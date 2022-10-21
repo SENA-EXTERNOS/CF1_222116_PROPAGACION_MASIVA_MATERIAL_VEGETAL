c28048d9c3bdc7ab1a4a0ddc7e4f553b51f5e0c6
santiagocadavid <santycadavid@hotmail.com>
2022-10-21 09:00:22 -0500

    Vinculación actividad

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -25,7 +25,7 @@
             .col-sm.mb-3.mb-sm-0
               p.fw-bold.mb-0 Descripción de la actividad didáctica.
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/actividad.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/actividades/CF1AD - Drag - 5/story.html')" target="_blank")
                 span Arrastrar y soltar
                 i.fas.fa-puzzle-piece
 
